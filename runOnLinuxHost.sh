2f36debe3123529361f4fd3d0311e343930fe3b4
Anton Krug <30338596+AntonKrug@users.noreply.github.com>
2018-06-25 15:34:33 +0100

    Allow distinguish between host and target execution

--- runOnLinuxHost.sh
+++ runOnLinuxHost.sh
@@ -2,6 +2,6 @@
 
 mkdir -p ./Debug
 rm Debug/host.elf 2>/dev/null
-gcc ./main.c -lm -o Debug/host.elf
+gcc ./main.c -lm -DEXIT_FROM_THE_INFINITE_LOOP -o Debug/host.elf
 chmod a+x Debug/host.elf
 ./Debug/host.elf
