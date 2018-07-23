264f923cfaa97710f3c4eb9d3f43c785dd85a107
Anton Krug <anton.krug@microsemi.com>
2018-07-23 17:25:34 +0100

    Cleaning both Release variants

--- tests/clean.sh
+++ tests/clean.sh
@@ -4,7 +4,9 @@ TESTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Clean the $TESTS_DIR Debug/Release folders"
 rm -r -f $TESTS_DIR/../Debug-hardfloat >/dev/null 2>&1
 rm -r -f $TESTS_DIR/../Debug-softfloat >/dev/null 2>&1
-rm -r -f $TESTS_DIR/../Release >/dev/null 2>&1
+rm -r -f $TESTS_DIR/../Release-hardfloat >/dev/null 2>&1
+rm -r -f $TESTS_DIR/../Release-softfloat >/dev/null 2>&1
 
 # clean all build folder from older project configurations
 rm -r -f $TESTS_DIR/../Debug* >/dev/null 2>&1
+rm -r -f $TESTS_DIR/../Release* >/dev/null 2>&1
