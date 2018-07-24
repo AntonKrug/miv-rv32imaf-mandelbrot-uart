bb38e69881e0e42196ac9b3204f96f6c0ad9b1a1
Anton Krug <anton.krug@microsemi.com>
2018-07-24 13:35:42 +0100

    Removed the hard coded checks for hardfloat and using the gcc supplied defines

--- tests/test-utils.c
+++ tests/test-utils.c
@@ -18,7 +18,7 @@ extern "C" {
 #endif
 
 
-// this variable can be set by different defines when using different compile
+// This variable can be set by different defines when using different compile
 // configurations and then it can detected with GDB on runtime and distinguish
 // what results the tests should use as the GDB test needs to have different
 // expected results for soft/hard floats as they slightly differ in the implementation.
@@ -29,7 +29,7 @@ unsigned int getConfigurationState() {
   ret |= CONFIGURATION_RISCV;
   ret |= CONFIGURATION_OPTIMALIZATION_0;
 
-#ifdef HARD_FLOAT_DESIGN
+#ifdef __riscv_flen
   ret |= CONFIGURATION_HARDFLOAT;
 #endif
 
@@ -56,10 +56,10 @@ void testAddToChecksumFloat(float value) {
 
 void testValidateBreak(unsigned int iteration, unsigned int blocking) {
 #ifdef GDB_TESTING
-  // when testing with gdb, place breakpoint here
+  // When testing with gdb, place breakpoint here
   volatile unsigned keepBlocking = blocking;
   printf("Test point reached \n");
-  while (keepBlocking);
+  while (keepBlocking); // Feel free to unblock it on runtime if needed
 #endif
 }
 
