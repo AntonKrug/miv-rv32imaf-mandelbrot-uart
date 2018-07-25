3dd8bc55c5d56b7737ac5b6fa67be161c017cd5f
Anton Krug <anton.krug@microsemi.com>
2018-07-25 14:23:08 +0100

    Native run works with bundled tests

--- runOnLinuxHost.sh
+++ runOnLinuxHost.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mkdir -p ./Debug
-rm Debug/host.elf 2>/dev/null
-gcc ./main.c -lm -DEXIT_FROM_THE_INFINITE_LOOP -o Debug/host.elf
-chmod a+x Debug/host.elf
-./Debug/host.elf
+mkdir -p ./Debug-native
+rm Debug-native/host.elf 2>/dev/null
+gcc ./main.c tests/test-utils.c -lm -DEXIT_FROM_THE_INFINITE_LOOP -o Debug-native/host.elf
+chmod a+x Debug-native/host.elf
+./Debug-native/host.elf
