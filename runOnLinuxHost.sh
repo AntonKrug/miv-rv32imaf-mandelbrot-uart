8994d5e204c73d8f7a81acfc94db1104a88911f9
Anton Krug <anton.krug@microsemi.com>
2018-07-25 14:51:10 +0100

    Styling

--- runOnLinuxHost.sh
+++ runOnLinuxHost.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 cd $SCRIPT_DIR
 
 mkdir -p ./Debug-native
 rm Debug-native/host.elf 2>/dev/null
-gcc ./main.c tests/test-utils.c -lm -DEXIT_FROM_THE_INFINITE_LOOP -o Debug-native/host.elf
+gcc ./main.c ./tests/test-utils.c -lm -DEXIT_FROM_THE_INFINITE_LOOP -o Debug-native/host.elf
 chmod a+x Debug-native/host.elf
 ./Debug-native/host.elf
