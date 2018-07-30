3487c7de1c91b41dcc179fdbab5dfcafa9db88ab
Anton Krug <anton.krug@microsemi.com>
2018-07-30 14:34:29 +0100

    Quiet

--- runOnLinuxHost.sh
+++ runOnLinuxHost.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 
 mkdir -p ./Debug-native
-rm Debug-native/host.elf 2>/dev/null
+rm Debug-native/host.elf >/dev/null 2>&1
 gcc ./main.c ./tests/test-utils.c -lm -DEXIT_FROM_THE_INFINITE_LOOP -o Debug-native/host.elf
 chmod a+x Debug-native/host.elf
 ./Debug-native/host.elf
