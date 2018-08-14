fdfee34ca00a3c15cf99593f2549fe9ffecb7acc
Anton Krug <anton.krug@microsemi.com>
2018-08-14 15:44:16 +0100

    Verbose description of kill actions

--- tests/killOpenOCD.sh
+++ tests/killOpenOCD.sh
@@ -11,12 +11,12 @@ function kill_all_existing {
     then
         if [[ "$(command -v sudo)" ]];
         then
-            # use regular kill if sudo is not present
+            echo "Using regular kill (sudo is not present)"
             kill $PIDS
         else
-            # use sudo if it's found on the system
+            echo "Using kill with sudo"
             sudo kill $PIDS
-        fi 
+        fi          
     fi
     sleep 3
 
@@ -26,10 +26,10 @@ function kill_all_existing {
         # agressive kill
         if [[ "$(command -v sudo)" ]];
         then
-            # use regular kill if sudo is not present
+            echo "Using agressive regular kill (sudo is not present)"
             kill -9 $PIDS
         else
-            # use sudo if it's found on the system
+            echo "Using agressive kill with sudo"
             sudo kill -9 $PIDS
         fi 
     fi
