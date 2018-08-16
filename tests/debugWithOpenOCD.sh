a656472697a1ff0989b5657f18574940e8687fa6
Anton Krug <anton.krug@microsemi.com>
2018-08-16 16:34:56 +0100

    Comments

--- tests/debugWithOpenOCD.sh
+++ tests/debugWithOpenOCD.sh
@@ -1,41 +1,57 @@
 #!/bin/bash
 
+
+# Use this hardcoded path if any other was not given by enviroment variables
 SC_DIR="${SC_DIR:-/opt/microsemi/softconsole/}"
 
+
+# Find location of this test directory no matter what current directory is
+# this might break between shells (tested with bash)
 TESTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
+# Making sure old OpenOCD is not running
 $TESTS_DIR/killOpenOCD.sh
 
-$SC_DIR/openocd/bin/openocd -f board/microsemi-riscv.cfg -c "echo openocd-started" &
-sleep 4 # give enough time for hardware to init
-echo "OpenOCD should be launched, start the gdb now:"
 
-echo "Go into first Debug folder I can find (make sure to run clean before this so there are no other folders present)"
+# Run OpenOCD on the background
+$SC_DIR/openocd/bin/openocd -f board/microsemi-riscv.cfg &
+
+
+# Give it enough time for hardware to init
+sleep 4 
+
+
+# Start GDB
+echo "OpenOCD should be launched, start the gdb now. Go into the first Debug"
+echo "folder I can find (make sure to run clean before this so there are no"
+echo "other folders present)."
 cd $TESTS_DIR/../Debug*
 $SC_DIR/riscv-unknown-elf-gcc/bin/riscv64-unknown-elf-gdb -x ../tests/gdb-test-checksum *.elf
 
-# Store the exit code
+
+# Store the exit code from GDB
 RESULT=$?
+
 
 # Kill OpenOCD first 
 $TESTS_DIR/killOpenOCD.sh
 
 
-# Cascade the gdb pass exit code out as a pass and all others as fails
-echo "Got $RESULT"
+# And then cascade the gdb pass exit code out as a pass and all other
+# codes as fails
+echo "Got code: $RESULT"
 if [ $RESULT == 149 ];
 then
     echo "Exiting as PASS"
     exit 0
 fi
 
-
 if [ $RESULT == 150 ];
 then
-    echo "Exiting as FAILURE on test failed (the checksum didn't matched)"
+    echo "Exiting as FAILURE as the test failed (the assertions failed)"
 else
-    echo "Exiting as FAILURE on the process failed (openocd/build/gdb)"
+    echo "Exiting as FAILURE as the process failed (openocd/build/gdb)"
 fi
 exit -1
 
